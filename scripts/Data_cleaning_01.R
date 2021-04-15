6bfc15623e7592fc894e108c0b0dee962d9a716b
yang <jane@example.com>
2021-04-15 11:11:13 -0400

    update

--- scripts/Data_cleaning_01.R
+++ scripts/Data_cleaning_01.R
@@ -14,7 +14,9 @@ library(tidyverse)
 ####read the data 
 library(here) # locate the file path
 data<- read.csv("inputs/DATA/questionnaire data.csv")
-####rename column names
+
+
+####rename demographics column names
 data<-
 data %>% 
   rename(
@@ -32,5 +34,50 @@ data %>%
 ####Remove useless columns
 questonnaire_data<-
   data %>%
-  select(-1)
+  select(-c(1,52,53,54,55,56,57,58))
+
+####rename variables column names
+questonnaire_data<-
+  questonnaire_data %>% 
+  rename(
+    Install_firewall=MD1,
+    BYOD=MD2,
+    Ignore_warnning=MD3,
+    Strong_pin=MD4,
+    Store_passwords=MD5,
+    update_OS=MD6,
+    public_WiFi=WN1,
+    unsecured_network=WN2,
+    network_control=WN3,
+    network_attack=WN4,
+    encrypted_transimittion=WN5,
+    unreliable_int=WN6,
+    cloud_resource=Cl1,
+    cloud_compute=Cl2,
+    remote_datastore=Cl3,
+    Data_regulation=Cl4,
+    multiple_login=Cl5,
+    weak_authentication=Cl6,
+    unknown_app=APP1,
+    update_ES=APP2,
+    Auto_update=APP3,
+    VPN=APP4,
+    third_party_app=APP5,
+    update=APP6,
+    single_authenticate=Da1,
+    audit_log=Da2,
+    access_right=Da3,
+    change_data=Da4,
+    malicious_attack=Da5,
+    Data_threats=Da6,
+    system_integrity=PS1,
+    system_availability=PS2,
+    system_confidentiality=PS3,
+    system_accountability=PS4,
+    system_nonrepudiation=PS5
+  )
+
+
+####Write into new csv
+write_csv(questonnaire_data, 'inputs/DATA/clean_data.csv')
 
